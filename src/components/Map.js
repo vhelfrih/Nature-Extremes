f0c9b087f87fac7f2ae33ff2d8b582b9d97d4ddc
Viktor <vhelfrih@gmail.com>
2022-01-28 11:52:44 +0100

    whitespace removal corrected

--- src/components/Map.js
+++ src/components/Map.js
@@ -7,42 +7,58 @@ import EventSelector from "./EventSelector";
 
 const Map = ({ eventData, center, zoom }) => {
   const [locationInfo, setLocationInfo] = useState(null);
-  
+
   const [selected, setSelected] = useState(null);
-  
+
   const handleSelect = (val) => {
     setSelected(val);
   };
-  
+
   const markers = eventData.map((ev, index) => {
     if (ev.categories[0].title === selected) {
       console.log(index);
-      return <LocationMarker key={index} iconType={selected} lat={ev.geometries[0].coordinates[1]} lng={ev.geometries[0].coordinates[0]}
-            onClick={() => setLocationInfo({ id: ev.id, title: ev.title, url: ev.sources[0].url, date: ev.geometries[0].date })} />
+      return (
+        <LocationMarker
+          key={index}
+          iconType={selected}
+          lat={ev.geometries[0].coordinates[1]}
+          lng={ev.geometries[0].coordinates[0]}
+          onClick={() =>
+            setLocationInfo({
+              id: ev.id,
+              title: ev.title,
+              url: ev.sources[0].url,
+              date: ev.geometries[0].date,
+            })
+          }
+        />
+      );
     }
     return null;
-  }) 
-    
-    return (
-      <div className="map">
+  });
+
+  return (
+    <div className="wrapper">
       <EventSelector onCheck={handleSelect} />
-      <GoogleMapReact
-        bootstrapURLKeys={{
-          key: process.env.REACT_APP_GMAP_KEY,
-        }}
-        defaultCenter={center}
-        defaultZoom={zoom}
+      <div className="map">
+        <GoogleMapReact
+          bootstrapURLKeys={{
+            key: process.env.REACT_APP_GMAP_KEY,
+          }}
+          defaultCenter={center}
+          defaultZoom={zoom}
         >
-        {markers}
-      </GoogleMapReact>
-      {locationInfo && 
-        <LocationInfoBox
-        info={locationInfo}
-        onClick={() => {
-          setLocationInfo(null);
-        }}
-        />
-        }
+          {markers}
+        </GoogleMapReact>
+        {locationInfo && (
+          <LocationInfoBox
+            info={locationInfo}
+            onClick={() => {
+              setLocationInfo(null);
+            }}
+          />
+        )}
+      </div>
     </div>
   );
 };
